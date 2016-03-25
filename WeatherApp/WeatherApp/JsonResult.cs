f4c30f7cc9159bcdb5714251a7aaafd9bd6daa83
kncxstudio <kncx@qq.com>
2016-03-25 16:21:30 +0800

    WeatherApp
    Json序列化 反序列化练习

---
+++ WeatherApp/WeatherApp/JsonResult.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    class JsonResult
+    {
+        JObject originalJson;
+        JArray jsonArray;
+        StringBuilder strBuilder;
+        private string info;
+        public string castDate { set; get; }
+        public string updateTime { set; get; }
+        public string minTemp { set; get; } = "0";
+        public string maxTemp { set; get; } = "0";
+        public string airQuality { set; get; }
+        public string hum { set; get; } = "0";
+        public string cond { set; get; }
+        public string condDay { set; get; }
+        public string condNight { set; get; }
+        public JsonResult(string json)
+        {
+            
+            originalJson = (JObject)JsonConvert.DeserializeObject(json);
+            jsonArray = TransJObjectToJArray(originalJson["HeWeather data service 3.0"][0]["daily_forecast"].ToString());
+            setSevenDayForecast();
+        }
+
+        public string getAllInfo()
+        {
+            return info;
+        }
+
+
+        private void setSevenDayForecast()
+        {
+            updateTime = originalJson["HeWeather data service 3.0"][0]["basic"]["update"]["loc"].ToString();
+            airQuality = originalJson["HeWeather data service 3.0"][0]["aqi"]["city"]["qlty"].ToString();
+            info += "\n\n数据更新时间：" + updateTime +"\n今日空气质量："+airQuality+"\n";
+            for (int i = 0; i < jsonArray.Count; i++)
+            {
+                JObject jObj = (JObject)jsonArray[i];
+
+                castDate = originalJson["HeWeather data service 3.0"][0]["daily_forecast"][i]["date"].ToString();
+                minTemp = originalJson["HeWeather data service 3.0"][0]["daily_forecast"][i]["tmp"]["min"].ToString();
+                maxTemp = originalJson["HeWeather data service 3.0"][0]["daily_forecast"][i]["tmp"]["max"].ToString();
+                hum = originalJson["HeWeather data service 3.0"][0]["daily_forecast"][i]["hum"].ToString();
+                condDay = originalJson["HeWeather data service 3.0"][0]["daily_forecast"][i]["cond"]["txt_d"].ToString();
+                condNight = originalJson["HeWeather data service 3.0"][0]["daily_forecast"][i]["cond"]["txt_n"].ToString();
+                //Debug.WriteLine(hum);
+                //Debug.WriteLine(condDay);
+                //Debug.WriteLine(condNight);
+                if (condDay.Equals(condNight))
+                {
+                    cond = condDay;
+                }
+                else
+                {
+                    cond = condDay + "  转  " + condNight;
+                }
+                info += "\n日期：" + castDate + "\n最低温度：" + minTemp + "℃\n最高温度：" + maxTemp + "℃\n" + cond + "\n相对湿度：" + hum+"%\n\n";
+
+            }
+        }
+        public static JArray TransJObjectToJArray(string json)
+        {
+            JArray jarr = (JArray)JsonConvert.DeserializeObject(json);
+            return jarr;
+        }
+
+    }
+
+
+  }
+
+
+
+
+
+
+
+    
+
---
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private static string cityName;
+        private static string url = "http://apis.baidu.com/heweather/weather/free";
+        private static string apiKey = "7d34a227739e836b3656e4c759c152f6";
+        private static string content;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void commitButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Debug.WriteLine("123123123123");
+            request(cityNameTextbox.Text);
+            //resultTB.Text = result;
+           
+        }
+        public void request(string city)
+        {
+            cityName = "city=" + city;
+            string strURL = url + '?' + cityName;
+            System.Net.HttpWebRequest request;
+            request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(strURL);
+            request.Method = "GET";
+            request.Headers["apikey"] = apiKey;
+            request.BeginGetResponse(callBack, request);
+        }
+
+        private async void callBack(IAsyncResult result)
+        {
+            
+           
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)result.AsyncState;
+                WebResponse webResponse = httpWebRequest.EndGetResponse(result);
+                using (Stream stream = webResponse.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    content = reader.ReadToEnd();
+                    JsonResult resultObj = new JsonResult(content);
+                     Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                     {
+                         resultTB.Text = resultObj.getAllInfo();
+                    }
+                    );
+                }
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            
+
+        }
+
+    }
+}
